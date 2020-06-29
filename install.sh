4ed270b297f6559e943ea2100f2b585a97b3c72f
Pierre <49315644+ech1@users.noreply.github.com>
2020-06-29 17:38:51 +0200

    update install script
    update install script

--- install.sh
+++ install.sh
@@ -6,7 +6,7 @@ sudo chmod +x /opt/torbridge/torbridge
 sudo ln -s /opt/torbridge/torbridge /usr/local/bin/torbridge
 
 cd torbridge
-sudo cp torbridge.service /etc/systemd/system/
+sudo cp /opt/torbridge/torbridge.service /etc/systemd/system/
 sudo systemctl start torbridge
 sudo systemctl status torbridge
 sudo systemctl enable torbridge
